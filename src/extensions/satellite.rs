c9bf46316c376d049a53ea9fda8e5be7eae358a5
Caleb Leinz <caleb.leinz@atlasspace.com>
2026-01-12 09:22:50 -0800

    feat: Add account and satellite configuration extensions to sat

--- src/extensions/satellite.rs
+++ src/extensions/satellite.rs
@@ -1,13 +1,46 @@
-use freedom_models::satellite::Satellite;
+use freedom_models::{
+    account::Account, satellite::Satellite, satellite_configuration::SatelliteConfiguration,
+};
 
-use crate::error::Error;
+use crate::{Api, error::Error};
 
 pub trait SatelliteExt {
     fn get_id(&self) -> Result<i32, Error>;
+
+    fn get_account<C>(
+        &self,
+        client: &C,
+    ) -> impl Future<Output = Result<Account, Error>> + Send + Sync
+    where
+        C: Api;
+
+    fn get_satellite_configuration<C>(
+        &self,
+        client: &C,
+    ) -> impl Future<Output = Result<SatelliteConfiguration, Error>> + Send + Sync
+    where
+        C: Api;
 }
 
 impl SatelliteExt for Satellite {
     fn get_id(&self) -> Result<i32, Error> {
         super::get_id("self", &self.links)
+    }
+
+    async fn get_account<C>(&self, client: &C) -> Result<Account, Error>
+    where
+        C: Api,
+    {
+        super::get_item("account", &self.links, client).await
+    }
+
+    async fn get_satellite_configuration<C>(
+        &self,
+        client: &C,
+    ) -> Result<SatelliteConfiguration, Error>
+    where
+        C: Api,
+    {
+        super::get_item("configuration", &self.links, client).await
     }
 }
